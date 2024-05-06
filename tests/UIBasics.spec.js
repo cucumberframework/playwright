d194d0748810362e34dd364842ca1bbaae9566bb
Rahul Barapatre <rahulbarapatre@IB-SEZ-LAP-846.local>
2024-05-06 16:26:11 +0530

    Lint fixes

--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -17,7 +17,7 @@ test('First playwright test execution', async ({page})=>
    await page.goto("https://sodev.ebixcrm.com/ms/index.htm");
    console.log(await page.title());
    await expect(page).toHaveTitle('SmartOffice');
-   //Switch to frame 
+   //Switch to frame of the creadentials 
    const loginPageFrame = await page.frameLocator('#frame_cache_0');
    const officeName=loginPageFrame.locator('#Office'); 
    const userName=loginPageFrame.locator("#User");
@@ -27,18 +27,24 @@ test('First playwright test execution', async ({page})=>
    const advancedSearch= page.locator("#searchAdvanced");
    const searchBox=page.locator("#search_value");
    const searchIcon=page.locator(".icon-search");
+
+   // Enter In correct office name , user name and password and then click on Submit
    await officeName.fill('bobbyms1')
    await userName.fill('Bobby');
    await password.fill('sod3mo');
    await submitButton.click();
+
+   // Check the error message which is displayed and assert the same 
    const errorMesage = await loginPageFrame.locator('form[id="loginForm"] div[style*="margin-bottom"] span[reason*="Your sign-in information"]').textContent();
    await expect(await loginPageFrame.locator('form[id="loginForm"] div[style*="margin-bottom"] span[reason*="Your sign-in information"]')).toContainText('Your sign-in information is incorrect. Please try again.');
    console.log(errorMesage);
 
-   // if i want to perform action on the first element  out of hte list of elements then 
+   // if I want to perform action on the first element  out of hte list of elements then 
    console.log(await loginPageFrame.locator('form[id="loginForm"] div[class="input-group addon-left"]').first().textContent()); //this will give the first element 
    console.log(await loginPageFrame.locator('form[id="loginForm"] div[class="input-group addon-left"]').last().textContent()); //this will give the last element from hte list of element
    console.log(await loginPageFrame.locator('form[id="loginForm"] div[class="input-group addon-left"]').nth(1).textContent()); //this will give the element of nth index 
+   
+   // Removing the Incorrect credentials and enter correct credentials 
    await officeName.fill("");
    await officeName.fill('bobbyms')
    console.log('Added correct user name');
@@ -59,7 +65,8 @@ test('First playwright test execution', async ({page})=>
    await searchIcon.click();
    const pageFrame= await page.frameLocator('#frame_cache_0');
    const topHeaderTab= await pageFrame.locator('td[reflinkbarid="_window_id1-linkbar"]');
-  //  await page.waitForLoadState('networkidle'); // it will wait for all network call to get complete 
+
+  //await page.waitForLoadState('networkidle'); // it will wait for all network call to get complete 
    await topHeaderTab.last().waitFor(); // it will wait for the last element to load from the list of element 
    console.log(await topHeaderTab.allTextContents());
 
@@ -73,9 +80,12 @@ test('First playwright test execution', async ({page})=>
    const frameOne = await page.frameLocator(".embed-responsive-item");
    const frameTwo= await frameOne.frameLocator("#ebRealModalFrame");
    await frameTwo.locator(".RADIO").first().waitFor();
-   //click on Radio button got Contacts
+
+   //click on Radio button got Contacts and assert that rasio button is checked 
    await frameTwo.locator(".RADIO").nth(1).click();
+   await expect(await frameTwo.locator(".RADIO").nth(1)).toBeChecked();
    await frameOne.frameLocator("#ebRealModalFrame").locator("#nm").fill("bobby");
+
    // console.log(await frameTwo.locator("#both_nm").isVisible());
    console.log(await frameTwo.locator("#ipID_65627").isVisible());
 
