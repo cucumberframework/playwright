ad7db9407228adb04dd0309d6c72b3a2a76a6890
Rahul Barapatre <rahulbarapatre@IB-SEZ-LAP-846.local>
2024-05-10 19:40:50 +0530

    Added changes done today

---
+++ tests/readWriteExcelSheet_6.spec.js
@@ -0,0 +1,27 @@
+const ExcelJS=require('exceljs');
+
+//read write data from excelJS
+//install exceljs dependency -- > npm install exceljs
+let output={row:-1,col:-1};
+async function excelTest(filePath,sheetName,textToReplace,newTextToadd){
+    const FileLoction=filePath;
+    const workbook=new ExcelJS.Workbook();
+    await workbook.xlsx.readFile(filePath);
+    const workSheet=workbook.getWorksheet(sheetName);
+    writeSheet(filePath,workbook,workSheet,textToReplace,newTextToadd)
+};
+async function writeSheet(filePath,workbook,workSheet,textToReplace,newTextToadd){
+    workSheet.eachRow((row,rowNumber)=>{
+        row.eachCell((cell,colNumber)=>{
+            if(cell.value===textToReplace){
+                output.row=rowNumber;
+                output.col=colNumber;   
+            };   
+        });
+    });
+    const cell =workSheet.getCell(output.row,output.col);
+    cell.value=newTextToadd;
+    await workbook.xlsx.writeFile(filePath);
+}
+excelTest('/Users/rahulbarapatre/Desktop/PlayWrightAutomation/externalFiles/playwright.xlsx',"Sheet1","NoWow","NoWow100");
+
