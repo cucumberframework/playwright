9581b6fd7abcf2ed7e27e698a6d40dd8d2b45287
Rahul Barapatre <rahulbarapatre@IB-SEZ-LAP-846.local>
2024-05-22 20:11:13 +0530

    JS basics

---
+++ tests/javaScriptBasics_Part2.js
@@ -0,0 +1,12 @@
+let marks=Array(6); //creating array with size of 6 
+let newMarks=new Array(20,10,11,3,15,16);
+console.log(newMarks[2]); //this will print the value present at index 2 
+
+let finalMarks=[1,2,3,4,5,6,7,8,9]; //third way to create the array 
+
+console.log(finalMarks[0]);  //will print the value present @ index 0
+console.log(finalMarks[1]);  //will print the value present at it dex 1
+
+finalMarks[3]=555;      //assigning value to specic index 
+console.log(finalMarks)
+console.log(finalMarks.length); //it will give length of the array 
