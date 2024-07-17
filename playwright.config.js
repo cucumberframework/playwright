eccb636f02f5a22ced557b2f43b57ec504079cbb
Rahul Barapatre <rahulbarapatre@IB-SEZ-LAP-846.local>
2024-07-17 17:59:00 +0530

    Added dockerCommands.txt file for running docker containor

--- playwright.config.js
+++ playwright.config.js
@@ -16,9 +16,9 @@ module.exports = defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 1 : 0,
+  retries: process.env.CI ? 1 : 2,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 8 : 5,
+  workers: process.env.CI ? 8 : 10,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
